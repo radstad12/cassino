ec4d9488db8c649501373daf4eba16b77f86b98f
radstad12 <radstad12@seznam.cz>
2026-09-25 16:57:40 +0200

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// GitHub Pages project site: https://radstad12.github.io/cassino/
-export default defineConfig({
-  base: '/cassino/',
-  plugins: [react()],
-});
